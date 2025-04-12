faee23e01472be2acba1d62982eab1bf45009b52
Gc20040702 <limag656@gmail.com>
2025-04-12 15:00:16 -0300

    Refatorando testes E2E

--- test/factories/make-answer.ts
+++ test/factories/make-answer.ts
@@ -5,6 +5,9 @@ import {
   Answer,
   AnswerProps,
 } from "@/domain/forum/enterprise/entities/answer";
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "@/infra/database/prisma/prisma.service";
+import { PrismaAnswerMapper } from "@/infra/database/prisma/mappers/prisma-answer-mapper";
 
 export function makeAnswer(
   override: Partial<AnswerProps> = {},
@@ -21,4 +24,21 @@ export function makeAnswer(
   );
 
   return answer;
+}
+
+@Injectable()
+export class AnswerFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaAnswer(
+    data: Partial<AnswerProps> = {}
+  ): Promise<Answer> {
+    const answer = makeAnswer(data);
+
+    await this.prisma.answer.create({
+      data: PrismaAnswerMapper.toPrisma(answer),
+    });
+
+    return answer;
+  }
 }
