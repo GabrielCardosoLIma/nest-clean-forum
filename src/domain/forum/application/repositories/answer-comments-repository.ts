ffc284480e85cfe3e33f94c6e091d61f2a3001e3
Isabella <117479295+isabellatressino@users.noreply.github.com>
2025-06-07 00:41:38 -0300

    Comentário da resposta com autor

--- src/domain/forum/application/repositories/answer-comments-repository.ts
+++ src/domain/forum/application/repositories/answer-comments-repository.ts
@@ -3,10 +3,17 @@ import { AnswerComment } from "../../enterprise/entities/answer-comment";
 
 export abstract class AnswerCommentsRepository {
   abstract findById(id: string): Promise<AnswerComment | null>;
+
   abstract findManyByAnswerId(
     id: string,
     params: PaginationParams
   ): Promise<AnswerComment[]>;
+
+  abstract findManyByAnswerIdWithAuthor(
+    answerId: string,
+    params: PaginationParams
+  ): Promise<CommentWithAuthor[]>;
+
   abstract create(answerComment: AnswerComment): Promise<void>;
   abstract delete(answerComment: AnswerComment): Promise<void>;
 }
